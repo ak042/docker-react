1c4382d3bfdb088448e7db42494f44ee3c49d977
aki042 <akio.iwase@gmail.com>
2018-11-24 07:41:53 -0800

    changed app text on feature 2

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Hello World
+            I was changed on the Feature branch
           </a>
         </header>
       </div>
